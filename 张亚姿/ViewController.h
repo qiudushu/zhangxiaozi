03f8903c140c154e7cc24716bb98f699c50b6268
chenhuiyi <1968281129@qq.com>
2016-05-19 09:47:41 +0800

    Initial Commit

---
+++ 张亚姿/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  张亚姿
+//
+//  Created by 邱读书 on 16/5/19.
+//  Copyright © 2016年 邱读书. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 张亚姿/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  张亚姿
+//
+//  Created by 邱读书 on 16/5/19.
+//  Copyright © 2016年 邱读书. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
